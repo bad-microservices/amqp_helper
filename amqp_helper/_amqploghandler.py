6067e3bef2b4bd0fa98eb16d27d059663914f783
Ole Hannemann <ole.hannemann@stadtwerke-schwedt.de>
2022-11-10 14:42:36 +0100

    regression fix v 0.0.8

--- amqp_helper/_amqploghandler.py
+++ amqp_helper/_amqploghandler.py
@@ -82,7 +82,7 @@ class LogProcess(mp.Process):
             channel = await connection.channel()
             while True:
                 msg = await self.asqueue.get()
-                routing_key = msg["level"]
+                routing_key = f"log_{msg['level']}"
                 await channel.default_exchange.publish(
                     aio_pika.Message(
                         body=json.dumps(msg).encode("utf-8"),
